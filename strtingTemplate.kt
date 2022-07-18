eeea6e53185a8b028dbdc4e1a17cbdbb067a71e2
iian armas <armas.cav@gmail.com>
2022-07-18 02:45:17 -0700

    Create strtingTemplate.kt

---
+++ strtingTemplate.kt
@@ -0,0 +1,9 @@
+var greet = "Hello" + " my name is "
+var name = "Iian"
+var mark = '!'
+
+val oneThird = 1.0 / 3.0
+
+fun main() {
+    println("Hello my name is $name and the answer is $oneThird" + mark)
+}
