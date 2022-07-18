4a4873ddebbd5c76686babdcc8691e071a09f06b
iian armas <armas.cav@gmail.com>
2022-07-17 20:31:39 -0700

    Create arithmetic.kt

---
+++ arithmetic.kt
@@ -0,0 +1,15 @@
+/* this is a comment
+    /* this is also a comment
+        this one is too
+    */
+ */
+
+fun main() {
+    println(2 + 6)  //8
+    println(10 - 2) //8
+    println(2 * 4)  //8
+    println(24 / 3) //8
+    println("%.0f".format(28.0 % 10.0))
+    println(14 shl 2) //shift left (binary)
+    println(14 shr 2) //shift right (binary)
+}
