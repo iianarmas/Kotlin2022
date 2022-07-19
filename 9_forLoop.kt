f31cdcafbad8855b4986416e95a5b61504a4a1a0
iian armas <armas.cav@gmail.com>
2022-07-19 11:48:01 +0800

    Update 9_forLoop.kt

--- 9_forLoop.kt
+++ 9_forLoop.kt
@@ -7,3 +7,23 @@ fun main() {
         println(sum)
     }
 }
+
+// =========================
+fun main() {
+    var sum = 1
+    var lastSum = 0
+    repeat(10) {
+        val temp = sum
+        sum += lastSum
+        lastSum = temp
+    }
+
+// =========================
+ fun main() {
+    var count = 10
+    var sum = 0
+    for (i in 1 .. count step 2){
+        sum += i
+        println(i)
+    }
+}
